84151669d6ee3f938b173a3cacbb35230bb18b2b
Eric Roos <ericroos@hey.com>
2022-02-03 19:43:00 -0600

    tables, show more tags

--- app/models/bank_transaction.rb
+++ app/models/bank_transaction.rb
@@ -1,3 +1,12 @@
 class BankTransaction < ApplicationRecord
   include Taggable
+
+  before_save :compute_md5
+
+  private
+
+  def compute_md5
+    self.md5 = Digest::MD5.hexdigest(md5_attrs.join("#"))
+  end
+
 end
