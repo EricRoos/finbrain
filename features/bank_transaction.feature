b53f87bebff976193fdb27ef6c88133291a2f016
Eric Roos <ericroos@hey.com>
2022-02-06 15:40:40 -0600

    ensure dom ids are set

--- features/bank_transaction.feature
+++ features/bank_transaction.feature
@@ -2,8 +2,8 @@ Feature: Bank Transaction
   @javascript
   Scenario: Displaying
     Given the user has a "bank transaction"
+    And the "bank transaction" has 5 "tags"
     And the user is on the "bank transaction" page
-    And the "bank transaction" has a list of "tags"
     Then the user sees the "description" of the "bank transaction"
     And the user sees the "posted at" of the "bank transaction"
     And the user sees the "total" of the "bank transaction"
@@ -11,6 +11,17 @@ Feature: Bank Transaction
     And the user should see "Suggested Tags"
     And the user should see "Recent Tags"
     And the user sees the "value" of every "tag"
+
+  @javascript
+  Scenario: Filtering
+    Given the user has a "bank transaction"
+    And the "bank transaction" has 1 "tags"
+    And the "tag" has a "value" of "foo"
+    And a tag exists
+    And the "tag" has a "value" of "bar"
+    And the user is on the "bank transactions" page
+    When the user clicks on "bar"
+    Then the user does not see the "bank transaction"
 
   @javascript
   Scenario: Adding a tag manually
--- features/step_definitions/generic.rb
+++ features/step_definitions/generic.rb
@@ -4,13 +4,10 @@ Given('the user has a list of {string}') do |class_name|
   @data[class_name] = FactoryBot.create_list(class_sym, 5).map(&:decorate)
 end
 
-Given('the {string} has a list of {string}') do |owner, class_name|
+Given('the user has a {string}') do |class_name|
   @data ||= {}
   class_sym = class_name.singularize.downcase.gsub(' ', '_').to_sym
-  FactoryBot.build_list(class_sym.to_sym, 5).each do |built|
-    @data[owner].send(class_sym.to_s.pluralize.to_sym) << built
-  end
-  @data[class_name] = @data[owner].send(class_sym.to_s.pluralize.to_sym).map(&:decorate)
+  @data[class_name] = FactoryBot.create(class_sym).decorate
 end
 
 
@@ -27,24 +24,34 @@ end
 
 Then('the user sees the {string} of every {string}') do |attribute, data_class|
   @data[data_class.pluralize].each do |model|
-    expect(page).to have_content(model.send(attribute.downcase.gsub(' ','_').to_sym))
+    within id: dom_id(model) do
+      expect(page).to have_content(model.send(attribute.downcase.gsub(' ','_').to_sym))
+    end
   end
 end
 
 Then('the user sees the {string} of the {string}') do |attribute, data_class|
   model = @data[data_class]
-  expect(page).to have_content(model.send(attribute.downcase.gsub(' ','_').to_sym))
+  within id: dom_id(model) do
+    expect(page).to have_content(model.send(attribute.downcase.gsub(' ','_').to_sym))
+  end
 end
 
 Then('the user should see {string}') do |content|
   expect(page).to have_content(content)
 end
 
-Given('the user has a {string}') do |class_name|
+
+Given('the {string} has {int} {string}') do |owner, count, class_name|
   @data ||= {}
   class_sym = class_name.singularize.downcase.gsub(' ', '_').to_sym
-  @data[class_name] = FactoryBot.create(class_sym).decorate
+  FactoryBot.build_list(class_sym.to_sym, count).each do |built|
+    @data[owner].send(class_sym.to_s.pluralize.to_sym) << built
+  end
+  @data[class_name] = @data[owner].send(class_sym.to_s.pluralize.to_sym).map(&:decorate)
+  @data[class_name.singularize] = @data[class_name].first if count == 1
 end
+
 
 When('the user fills in {string} with {string}') do |field, value| 
   fill_in field, with: value
@@ -52,5 +59,30 @@ end
 
 When('the user presses {string}') do |button|
   click_on button
+end
+
+Given('the {string} has a {string}') do |owner, class_name|
+  obj = FactoryBot.build(class_name.downcase.underscore.to_sym)
+  @data[owner].send("#{class_name.downcase.underscore}=".to_sym, obj)
+  @data[owner.save]
+end
+
+Given('the {string} has a {string} of {string}') do |owner, attribute, val|
+  updates = {}
+  updates[attribute.to_sym] = val
+  updated = @data[owner].update(updates)
+end
+
+Given('a tag exists') do
+  @data['tag'] = FactoryBot.create(:tag)
+end
+
+When('the user clicks on {string}') do |string|
+  click_on string
+end
+
+Then('the user does not see the {string}') do |data_class|
+  model = @data[data_class]
+  expect{ page.find(id: dom_id(model)) }.to raise_error(Capybara::ElementNotFound)
 end
 
