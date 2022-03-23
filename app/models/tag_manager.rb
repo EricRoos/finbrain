ecf8bb58397fd8f1dda184366d1cc0ce6d1ab012
Eric Roos <ericroos@hey.com>
2022-03-23 09:05:26 -0500

    ui flows

--- app/models/tag_manager.rb
+++ app/models/tag_manager.rb
@@ -10,4 +10,15 @@ module TagManager
     end
   end
 
+  def self.add_associated_tag(first_tag, associated_tag)
+    tag = Tag.where(value: first_tag).first
+
+    new_tag = Tag.where(value: associated_tag).first
+    new_tag = Tag.create(value: associated_tag) if new_tag.nil?
+
+    new_tag_relations = tag.tag_relations.map do |tag_relation|
+      { taggable_type: tag_relation.taggable_type, taggable_id: tag_relation.taggable_id, tag_id: new_tag.id }
+    end
+    TagRelation.insert_all new_tag_relations
+  end
 end
